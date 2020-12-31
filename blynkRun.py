c05d8ce33f96e0f81d3b98bff3a4d42782e342f4
KathleenMK <kathleenmcck@gmail.com>
2020-12-31 16:34:15 +0000

    Added timer events and comments

--- blynkRun.py
+++ blynkRun.py
@@ -1,3 +1,11 @@
+""" Main script required to interface with the BLYNK app "Depends on Weather"  
+Purpose of script is to run the dryingWindowCalc.py script to identify drying windows,
+    send that data to the Blynk app periodically
+Also takes readings from a rain sensor to alert if rain is detected
+Also takes readings from a hummidity/temperature sensor and sends current conditions to the Blynk app
+Determines what and when notifications are sent
+displays on sense hat and takes input from the sensehat joystick
+"""
 
 import requests   # https://www.w3schools.com/python/ref_requests_get.asp 28Nov20
 import json
@@ -9,25 +17,36 @@ import time
 from time import sleep
 from gpiozero import InputDevice
 import dryingWindowCalc
+import otherWindowCalc
 #import presenceCheck
 import haversine as hs #sudo pip3 install haversine 19Dec20 https://towardsdatascience.com/calculating-distance-between-two-geolocations-in-python-26ad3afe287b 17Dec20
 import blynktimer
-import random
+#import random
 
 #from dryingCalc import dryingWindows #https://stackoverflow.com/questions/36925670/calling-variables-in-another-module-python 08Dec20
+g=(0,255,0)
+b=(0,0,0)
+r=(255,0,0)
+
+onPixels = [
+    b, b, b, b, b, b, b, b,
+    r, g, g, g, b, b, r, b,
+    b, r, b, b, g, r, b, b,
+    b, g, r, b, r, g, b, b,
+    b, g, b, r, b, g, b, b,
+    b, g, r, b, r, g, b, b,
+    b, r, b, b, g, r, b, b,
+    r, g, g, g, b, b, r, b
+]
 
 no_rain = InputDevice(18) #is true if not raining
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
-blue = (0,0,255)
-red = (255,0,0)
-green = (0,255,0)
 sense = SenseHat()
 sense.clear()
-sense.show_message("On!", text_colour = blue)
-sense.show_letter("*", text_colour = blue)
+sense.show_message("On!", text_colour = (0,0,255))
+sense.set_pixels(onPixels)
 #homeInd = len(presenceCheck.find_devices())
-
 
 BLYNK_AUTH = 'LTVERrZ1k3ghsbg-6wzZuZQRZWfwBbpS'
 
@@ -37,11 +56,35 @@ blynk = blynklib.Blynk(BLYNK_AUTH)
 # create timers dispatcher instance
 timer = blynktimer.Timer()
 
+
+WRITE_EVENT_PRINT_MSG = "[virtual_write] Pin: V{}, Value: '{}'"
+
+# Upon the running if this script, setting drying button (V5) to OFF, using timer
+    # using timer examples at https://github.com/blynkkk/lib-python/blob/master/examples/08_blynk_timer.py visited 28Dec20
+@timer.register(vpin_num=5, interval=1, run_once=True)  # run at 1 sec, once only
+def write_to_virtual_pin(vpin_num=1):
+    value = 0 #off position
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
+    blynk.virtual_write(vpin_num, 0)
+
+# When script is run, setting the virtual pin values on the Blynk app to be blank
+@timer.register(vpin_num=2, interval=1, run_once=True)
+@timer.register(vpin_num=3, interval=1, run_once=True)
+@timer.register(vpin_num=4, interval=1, run_once=True)
+@timer.register(vpin_num=8, interval=1, run_once=True)
+@timer.register(vpin_num=10, interval=1, run_once=True)
+@timer.register(vpin_num=11, interval=1, run_once=True)
+@timer.register(vpin_num=12, interval=1, run_once=True)
+def write_to_virtual_pin(vpin_num=1):
+    value = str(" ")
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
+    blynk.virtual_write(vpin_num, value)
+
 global homeInd
 homeInd = True
 
 global drying
-drying = None
+drying = False
 
 global rain
 rain = None
@@ -83,10 +126,10 @@ def notifyNight():
     #print(str(drying)+str(homeInd)+str(nightInd))
     global nightNotifyDue
     if drying and homeInd and nightInd and nightNotifyDue:
-        blynk.notify("Its nightfall. what about those clothes?")
+        blynk.notify("Its night! What about those clothes?")
         nightNotifyDue = False
     elif drying and nightInd and nightNotifyDue:
-        blynk.email('kathleenmcck@gmail.com', 'Its Nightfall and the clothes are still out', 'Please bring in the clothes')
+        blynk.email('kathleenmcck@gmail.com', 'Its Night time and the clothes are still out', 'Please bring in the clothes')
         print(nightNotifyDue)
         nightNotifyDue = False
         print(nightNotifyDue)
@@ -96,33 +139,32 @@ def notifyNight():
 
 
 
-WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_WRITE] Pin: V{} Value: '{}'"
-
-
-@timer.register(vpin_num=5, interval=1, run_once=True)
+@timer.register(vpin_num=10, interval=62, run_once=False)
 def write_to_virtual_pin(vpin_num=1):
-    value = 0 #HIGH #str(" ") #random.randint(0, 20)
-    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
-    blynk.virtual_write(vpin_num, 0)
-
-#https://github.com/blynkkk/lib-python/blob/master/examples/08_blynk_timer.py 28Dec20
-# Code below: register two timers for different pins with different intervals
-# run_once flag allows to run timers once or periodically
-@timer.register(vpin_num=2, interval=1, run_once=True)
-@timer.register(vpin_num=3, interval=1, run_once=True)
-@timer.register(vpin_num=4, interval=1, run_once=True)
-@timer.register(vpin_num=8, interval=1, run_once=True)
-@timer.register(vpin_num=10, interval=1, run_once=True)
-@timer.register(vpin_num=11, interval=1, run_once=True)
-@timer.register(vpin_num=12, interval=1, run_once=True)
-def write_to_virtual_pin(vpin_num=1):
-    value = str(" ") #random.randint(0, 20)
-    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
+    otherWindowCalc.calcOtherWindows() #no account if data is not available... potential issue
+    currentTime = datetime.datetime.now().strftime(" %d/%m/%Y %H:%M:%S")
+    value = otherWindowCalc.eightAM[0][1]+str(": ")+str(otherWindowCalc.eightAM[0][4])+str(" rainfall, ")+str(otherWindowCalc.eightAM[0][5])+str("C temp...")+str(otherWindowCalc.eightAM[0][6])
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value)+str(currentTime))
     blynk.virtual_write(vpin_num, value)
+    if float(otherWindowCalc.eightAM[0][4]) == 0:
+      blynk.set_property(vpin_num, 'color', '#08FF08')
+    else:
+      blynk.set_property(vpin_num, 'color', '#FF0000')
 
-@timer.register(vpin_num=10, interval=10, run_once=False)
-@timer.register(vpin_num=11, interval=11, run_once=False)
-@timer.register(vpin_num=12, interval=12, run_once=False)
+@timer.register(vpin_num=11, interval=63, run_once=False)
+def write_to_virtual_pin(vpin_num=1):
+    otherWindowCalc.calcOtherWindows() #no account if data is not available... potential issue
+    currentTime = datetime.datetime.now().strftime(" %d/%m/%Y %H:%M:%S")
+    value = otherWindowCalc.twoPM[0][1]+str(": ")+str(otherWindowCalc.twoPM[0][4])+str(" rainfall, ")+str(otherWindowCalc.twoPM[0][5])+str("C temp...")+str(otherWindowCalc.twoPM[0][6])
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value)+str(currentTime))
+    blynk.virtual_write(vpin_num, value)
+    if float(otherWindowCalc.twoPM[0][4]) == 0:
+      blynk.set_property(vpin_num, 'color', '#08FF08')
+    else:
+      blynk.set_property(vpin_num, 'color', '#FF0000')
+
+
+@timer.register(vpin_num=12, interval=64, run_once=False)
 def write_to_virtual_pin(vpin_num=1):
     value = str("new calc") #random.randint(0, 20)
     print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
@@ -130,43 +172,28 @@ def write_to_virtual_pin(vpin_num=1):
 
 
 
-
-@timer.register(vpin_num=3, interval=25, run_once=False)
+@timer.register(vpin_num=2, interval=60, run_once=False)
 def write_to_virtual_pin(vpin_num=1):
     dryingWindowCalc.calcDryingWindows()
-    value = dryingWindowCalc.dryingWindows[1][0]+str(" @ ")+dryingWindowCalc.dryingWindows[1][1]+str(" for ")+str(dryingWindowCalc.dryingWindows[1][3])+str(" hours!")
-    #str("boomiest") #random.randint(0, 20)
-    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
+    currentTime = datetime.datetime.now().strftime(" %d/%m/%Y %H:%M:%S")
+    value = dryingWindowCalc.dryingWindows[0][0]+str(" @ ")+dryingWindowCalc.dryingWindows[0][1]+str(" for ")+str(dryingWindowCalc.dryingWindows[0][3])+str(" hours!")
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value)+str(currentTime))
     blynk.virtual_write(vpin_num, value)
 
 
-# Blynk event that displays next drying window on app
-# V2 and V3 seems to work best when blynk reading rate is not equal
-@blynk.handle_event('read V2')
-def read_virtual_pin_handler(pin):
-    dryingWindowCalc.calcDryingWindows()    #placed within handler so that it recalculates as Blynk is running
+
+@timer.register(vpin_num=3, interval=61, run_once=False)
+def write_to_virtual_pin(vpin_num=1):
+    dryingWindowCalc.calcDryingWindows()
     currentTime = datetime.datetime.now().strftime(" %d/%m/%Y %H:%M:%S")
-    nextWindow=dryingWindowCalc.dryingWindows[0][0]+str(" @ ")+dryingWindowCalc.dryingWindows[0][1]+str(" for ")+str(dryingWindowCalc.dryingWindows[0][3])+str(" hours!") #int duration needed to be converted to str
-    print('V2 Read: '+str(nextWindow)+str(currentTime))
-    blynk.virtual_write(pin, nextWindow)
-    
-        
-# Blynk event that displays following drying window on app    
-@blynk.handle_event('read V3')
-def read_virtual_pin_handler(pin):
-    #dryingCalc.calcDryingWindows()
-    currentTime = datetime.datetime.now().strftime(" %d/%m/%Y %H:%M:%S")
-    followingWindow=dryingWindowCalc.dryingWindows[1][0]+str(" @ ")+dryingWindowCalc.dryingWindows[1][1]+str(" for ")+str(dryingWindowCalc.dryingWindows[1][3])+str(" hours!")
-    print('V3 Read: '+str(followingWindow)+str(currentTime)) 
-    """ if(runCount==1):
-        blynk.virtual_write(pin, " ")
-        print('V3 runCount is '+str(runCount))
-    else: """
-    blynk.virtual_write(pin, followingWindow)
-      
+    value = dryingWindowCalc.dryingWindows[1][0]+str(" @ ")+dryingWindowCalc.dryingWindows[1][1]+str(" for ")+str(dryingWindowCalc.dryingWindows[1][3])+str(" hours!")
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value)+str(currentTime))
+    blynk.virtual_write(vpin_num, value)
 
 
-# does not engage until pressed on app
+
+
+
 @blynk.handle_event('write V5')
 def write_virtual_pin_handler(pin, value):
     currentTime = datetime.datetime.now().strftime(" %d/%m/%Y %H:%M:%S")
@@ -174,15 +201,14 @@ def write_virtual_pin_handler(pin, value):
     global drying  #https://www.w3schools.com/python/python_variables_global.asp  03Dec20
     if value[0]=="1":
         drying=True
-        print(str("drying is in progress")+str(drying))
-        sense.show_letter("D", text_colour = green)
+        print(str("drying is in progress, drying variable is ")+str(drying))
+        sense.show_letter("D", text_colour = g)
     else:
         drying=False
-        print(str("drying not in progress")+str(drying))
-        sense.show_letter("*", text_colour = blue)
+        print(str("drying not in progress, , drying variable is ")+str(drying))
+        sense.set_pixels(onPixels)
+        
 
-#print(str(drying))
-#https://raspi.tv/2017/make-a-rain-alert-system-with-raspberry-pi 02Dec20
 #https://www.thegeekpub.com/236867/using-the-dht11-temperature-sensor-with-the-raspberry-pi/ as at 03Dec20
 @blynk.handle_event('read V4')
 def read_virtual_pin_handler(pin):
@@ -190,32 +216,17 @@ def read_virtual_pin_handler(pin):
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     #humidity = str("Humidity: ")+str(round(sense.get_humidity(),2))+str("%")
     global rain
-    if not no_rain.is_active: #and drying==0: #so drying is not not in progress, so in progress
+    if not no_rain.is_active: ##https://raspi.tv/2017/make-a-rain-alert-system-with-raspberry-pi 02Dec20
        rain=True # should be True
-       """ if drying==True:
-         blynk.set_property(pin, 'color', '#FF0000')
-         sense.show_letter("R", text_colour = red)
-         print(homeInd)
-         if (homeInd):
-             blynk.notify(rain)  #https://github.com/blynkkk/lib-python 01Dec20
-         else:
-             blynk.email('kathleenmcck@gmail.com', 'Its Raining', 'Please bring in the clothes')
-       else:
-         blynk.set_property(pin, 'color', '#C0C0C0')
-         sense.show_letter("*", text_colour = blue)
-         #sense.show_message("No Rain", text_colour = green) """
+       rainTxt="Rain"
+       blynk.set_property(pin, 'color', '#FF0000')      
     else:
        rain=False
-       """ if drying==1:
-         blynk.set_property(pin, 'color', '#00FF00')
-         sense.show_letter("D", text_colour = green)
-       else:
-         blynk.set_property(pin, 'color', '#C0C0C0')
-         sense.show_letter("*", text_colour = blue) """
-    #humidityR = ("Temp:{0:0.1f}C Humidity:{1:0.1f}%".format(temperature, humidity))+str(rain)
-    humidityR = str("Humidity: ")+format(humidity)+str("%, ")+str(rain)  #seem to need format() for humidity when combined with str
-    print('V4 Read: '+str(humidityR)+str(drying)+str(currentTime))
-    blynk.virtual_write(pin, humidityR)
+       rainTxt="Dry"
+       blynk.set_property(pin, 'color', '#23C48E')
+    current = ("Humidity:{1:0.0f}%, Temp:{0:0.0f}C, ".format(temperature, humidity)+str(rainTxt))
+    print('V4 Read: '+str(current)+str(currentTime))
+    blynk.virtual_write(pin, current)
 
 
 #phone location needs to be one for this event to engage
@@ -230,38 +241,30 @@ def write_virtual_pin_handler(pin, value):
     #phone = (52.1742080,-7.1585798)
     phone = (latitude,longitude)
     global distanceFromHome
-    distanceFromHome = hs.haversine(home,phone)
+    distanceFromHome = round(hs.haversine(home,phone),4)
     global homeInd
     if distanceFromHome > 1:
         homeInd = False
     else:
         homeInd = True
-    print(phone)
-    print(distanceFromHome)
-    print(homeInd)
-
-
-
-
+    print('V6 Read: phone:'+str(phone)+' distanceFromHome:'+str(distanceFromHome)+' homeInd:'+str(homeInd))
+    
 
 # Blynk event that displays following drying window on app    
 @blynk.handle_event('read V8')
 def read_virtual_pin_handler(pin):
-    #dryingCalc.calcDryingWindows()
-    #currentTime = datetime.datetime.now().strftime(" %d/%m/%Y %H:%M:%S")
-    #followingWindow=dryingWindowCalc.dryingWindows[1][0]+str(" @ ")+dryingWindowCalc.dryingWindows[1][1]+str(" for ")+str(dryingWindowCalc.dryingWindows[1][3])+str(" hours!")
-    print('V8 Read: '+str(rain)+str(nightInd))
+    print('V8 Read: rain:'+str(rain)+' nightInd:'+str(nightInd))
     if not(drying):
         blynk.virtual_write(pin, " ")
     elif rain and nightInd:
-        blynk.virtual_write(pin, "It's raining and Nighttime")
+        blynk.virtual_write(pin, "It's raining and Night time")
     elif rain:
         blynk.virtual_write(pin, "It's raining")
     elif nightInd:
-        blynk.virtual_write(pin, "It's Nighttime")
+        blynk.virtual_write(pin, "It's Night time")
     else:
         blynk.virtual_write(pin, " ")
-        
+
 
 while True:
     timer.run() #only works if there is a timer event repeating
@@ -269,3 +272,17 @@ while True:
     nightIndCheck()
     notifyRain()
     notifyNight()
+    for event in sense.stick.get_events():
+       print(event.direction, event.action)
+       if event.action == "pressed":
+           if drying:
+            drying=False
+            blynk.virtual_write(5, 0)
+            print(str("drying not in progress")+str(drying))
+            sense.set_pixels(onPixels)
+           else:
+            drying=True
+            blynk.virtual_write(5, 1)
+            print(str("drying in progress")+str(drying))
+            sense.show_letter("D", text_colour = g)
+           
--- dryingWindowCalc.py
+++ dryingWindowCalc.py
@@ -1,39 +1,58 @@
+"""
+dryingWindowCalc.py script
 
-import requests   # https://www.w3schools.com/python/ref_requests_get.asp 28Nov20
+Defines the function calcDryingWindows for use by blynkRun.py:
+Step 1) gets the local weather forecast from public web api
+Step 2) determines the hours in the forecast that meet the drying criteria
+Step 3) assigns a window # if the hours are adjacent from the above step
+Step 4) create and populate dryingWindows array with day, start time, day index and duration of window
+"""
+
+import requests   # https://www.w3schools.com/python/ref_requests_get.asp visited 28Nov20
 import json
 
 def calcDryingWindows():
  print("just begining the calculation of drying windows")
-# api
-# global URL
+
+# STEP 1: gets the local weather forecast from public web api
+# ----------------------------------------------------------- 
+
+# global URL, local weather forecast api taken from met.ie website 
  URL = 'https://prodapi.metweb.ie/weather/details/52.16235/-7.15244';
-#sending get request and saving response as response object
-# r below is a python list, of length 97ish
-# global r 
- r = requests.get(url = URL).json()
- 
-#blynk.notify("boom")
-#https://www.w3schools.com/python/python_tuples.asp
-#Tuples are used to store multiple items in a single variable.
-#A tuple is a collection which is ordered and unchangeable.
-#def calcDryingHours():
- # getForecast()
+
+# sending get request and saving response as response object
+ r = requests.get(url = URL).json() # r below is a python list, of length 97ish
+
+
+# STEP 2: determines the hours in the forecast that meet the drying criteria
+# -------------------------------------------------------------------------- 
+# rainfall zero
+# humidity <= 90% (believe 70% is a better measure however using higher figure to show workings)
+# time of day between 7am and 7pm
+# json data being transformed into tuples (#https://www.w3schools.com/python/python_tuples.asp)
+    #Tuples are used to store multiple items in a single variable.
+    #A tuple is a collection which is ordered and unchangeable.
  i=0
- dryingHours=[]
- while i < len(r):
-      #print("in the dryingHours loop")
-      if float(r[i]["rainfall"]) == 0 and float(r[i]["humidity"]) <= 95 and r[i]["time"] >= "07:00" and r[i]["time"] < "21:00": #expected to use humidity 70%, time 8 to 8
+ global dryingHours
+ dryingHours=[] #defining an empty array to be populated with hours that meet the drying criteria
+ while i < len(r):  #while loop to check each entry in the above response "r"
+      if float(r[i]["rainfall"]) == 0 and float(r[i]["humidity"]) <= 95 and r[i]["time"] >= "07:00" and r[i]["time"] < "19:00": 
         dryingHours.append(tuple((i,r[i]["shortDayName"],r[i]["time"],r[i]["humidity"],r[i]["rainfall"],r[i]["dayNumber"])))
       i = i + 1
+
+
+# STEP 3: Assigns a window # if the hours are adjacent from the above step
+# ------------------------------------------------------------------------
+# dyingHours array from above step being looped through to determine whether drying hours are adjacent using index from response object,
+# if so deemed the same window and assigned the same window #
+# windowcalc array defined and populated with drying hours and assigned window number as tuples
 #def calcStepWindows():
  j=0
  windowNo=1
+ global windowCalc
  windowCalc=[]
-  #dryingWindows.append(tuple((0,dryingHours[0][0])))
-  #data in later days is not per hour, but every 3 hours, what does that mean for the windows
-  #therefore keeping the original r index to calculate the length of the window
+#data in later days is not per hour, but every 3 hours, therefore keeping the original r index to calculate the length of the window
  while j < len(dryingHours):
-    #print("in the window calc step")
     if j == 0:
       windowCalc.append(tuple((dryingHours[j][0],dryingHours[j][1],dryingHours[j][2],dryingHours[j][5],windowNo)))
     else:
@@ -43,58 +62,47 @@ def calcDryingWindows():
         windowNo = windowNo + 1
         windowCalc.append(tuple((dryingHours[j][0],dryingHours[j][1],dryingHours[j][2],dryingHours[j][5],windowNo)))
     j = j + 1
-#so windowCalc list contains the following 5 pieces of data
-# original list index
-# short day name
-# time
-# day number
-# window number
-# would like to output time, day and number of hours in window
-# can't update a tuple, only adding when all info known
-#def calcDryingWindows():
-  #getForecast()
- # calcDryingHours()
-  #calcStepWindows()
+
+
+
+# STEP 4: Create and populate dryingWindows array with day, start time, day index and duration of window
+# ------------------------------------------------------------------------------------------------------
+# windowCalc above contains the following 5 pieces of data: 
+# original list index; short day name; time; day number; window number 
+# can't update a tuple, therefore only adding when all info known
  k=0
- global dryingWindows #make gloabl to aloow import into blynkData file
+ global dryingWindows #make global to allow import into blynkRun file
  dryingWindows=[]
  while k < len(windowCalc):
-    #print("in the final calc step")
-    if k == 0:
+    if k == 0:  #first window
       day=windowCalc[k][1]
       startTime=windowCalc[k][2]
       startHour=(startTime[0]+startTime[1]) #time always on the hour, taking the character at index 0 and 1
       dayNo=windowCalc[k][3]
-      #print(k)
-      #dryingWindowSum.append(tuple((dryingWindows[k][1],dryingWindows[k][2],dryingWindows[k][3],dryingWindows[k][4],1))) #last value to be hours in window
     else:
-      if windowCalc[k][4] != windowCalc[k-1][4]:   #so if the window number is the same as the previous
-        endTime=windowCalc[k-1][2]  #end hour could reference the original list and check whether the interval is > 1
-        endHour=(endTime[0]+endTime[1])
-        #print(startHour)     
-        #print(endHour)
-        duration=(int(endHour)-int(startHour)+1)
-        dryingWindows.append(tuple((day,startTime,dayNo,duration)))
+      if windowCalc[k][4] != windowCalc[k-1][4]:   # window number is not the same as the previous
+        # therefore previous window can be closed
+        endTime=windowCalc[k-1][2]  # end time is taken from the previous entry
+        endHour=(endTime[0]+endTime[1]) #time always on the hour, taking the character at index 0 and 1
+        duration=(int(endHour)-int(startHour)+1) #duration is assuming 1 hour added to end hour
+        dryingWindows.append(tuple((day,startTime,dayNo,duration))) # append complete window information to dryingWindows array
+        # and new window processed (similar to where k is 0 above)
         day=windowCalc[k][1]
         startTime=windowCalc[k][2]
         startHour=(startTime[0]+startTime[1]) #time always on the hour, taking the character at index 0 and 1
         dayNo=windowCalc[k][3]
-        if k == len(windowCalc)-1:
+      if k == len(windowCalc)-1:  #last entry in the windowcalc array
             endTime=windowCalc[k][2]  #end assumed to be that of the last forcast, so 1 later than the last drying hour
             endHour=(endTime[0]+endTime[1]) 
-            #print(startHour)     
-            #print(endHour)
             duration=(int(endHour)-int(startHour)+1)
-            dryingWindows.append(tuple((day,startTime,dayNo,duration))) #duration is incorrect as it hasn't been updated
-            #print("the end")
+            dryingWindows.append(tuple((day,startTime,dayNo,duration)))
     k = k + 1
 
 
 #calcDryingWindows()
-
- 
-
-
-
 #print(len(dryingHours))
+#print(dryingHours)
+#print(len(windowCalc))
+#print(windowCalc)
 #print(dryingWindows)
+
